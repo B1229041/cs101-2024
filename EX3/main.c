74d7a7ffc7301cf59b77005284cd285ca7b885aa
B1229041 <144075718+B1229041@users.noreply.github.com>
2024-03-14 15:39:29 +0800

    Update main.c

--- EX3/main.c
+++ EX3/main.c
@@ -1,16 +1,1 @@
-#include <stdio.h>
-#include <string.h>
 
-int main() {
-    char str[101];
-    scanf("%100s", str);
-
-    int length = strlen(str);
-
-    for (int i = length - 1; i >= 0; i--) {
-        printf("%c", str[i]);
-    }
-    printf("\n");
-
-    return 0;
-}
